1925566e52c36c314c6766d8d75290479ad1d6ad
laith19-meet <laith19@meet.mit.edu>
2017-07-30 18:30:43 +0300

    Stopping here

---
+++ stringfun.py
@@ -0,0 +1,11 @@
+def string_test (s) :
+    if len(s) > 2 and s[0] ==s[-1] :
+        return bool("true")
+    else :
+        return ("false")
+        
+def add_x (s) :
+    return ('x'+ s +'x')
+
+
+
